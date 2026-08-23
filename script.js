e98cb49abe0a80404503ae95013ab241c4cb5364
Isadoce <ortizisadora52@gmail.com>
2026-08-23 15:37:23 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f5f2ed;
+    text-align: center;
+    padding: 80px;
+}
+
+h1 {
+    color: #4b1f2f;
+    font-size: 40px;
+}
+
+p {
+    color: #333333;
+    font-size: 18px;
+}
+
+button {
+    background-color: #4b1f2f;
+    color: white;
+    border: none;
+    padding: 12px 24px;
+    border-radius: 5px;
+    font-size: 16px;
+}
